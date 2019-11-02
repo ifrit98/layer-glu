bcf5948e558a20254bb11f3d7409ff7dd3ea3e97
home <ifrit98@gmail.com>
2019-11-02 16:53:56 -0400

    add R src

---
+++ layer-glu.R
@@ -0,0 +1,288 @@
+
+
+my_kernel_initializer <- 
+  function(shape, stddev = NULL, dtype = NULL) {
+    tf$keras$backend$random_normal(shape = shape, 
+                                   stddev = stddev, 
+                                   dtype = dtype)
+  }
+
+
+
+GatedLinearUnit <-
+  R6::R6Class(
+    "GatedLinearUnit",
+    
+    inherit = KerasLayer,
+    
+    public = list(
+      filters = NULL,
+      kernel_size = NULL,
+      kernel_initializer = NULL,
+      kernel_regularizer = NULL,
+      bias_initializer = NULL,
+      bias_regularizer = NULL,
+      linear_kernel = NULL,
+      gated_kernel = NULL,
+      linear_bias = NULL,
+      gated_bias = NULL,
+      kernel_shape = NULL,
+      
+      initialize = function(filters,
+                            kernel_size,
+                            kernel_initializer,
+                            kernel_regularizer,
+                            bias_initializer,
+                            bias_regularizer) {
+        self$filters <- filters
+        self$kernel_size <- kernel_size
+        self$kernel_initializer <- kernel_initializer
+        self$kernel_regularizer <- kernel_regularizer
+        self$bias_initializer <- bias_initializer
+        self$bias_regularizer <- bias_regularizer
+      },
+      
+      build = function(input_shape) {
+        
+        if (length(input_shape) == 1L)
+          input_shape <- tf$TensorShape(input_shape[[1]])
+        
+        self$kernel_shape <-
+          list(self$kernel_size,
+               input_shape[[length(input_shape)]],
+               self$filters)
+        
+        self$linear_kernel <- self$add_weight(
+          name = 'linear_kernel',
+          shape = self$kernel_shape,
+          initializer = self$kernel_initializer,
+          regularizer = self$kernel_regularizer,
+          trainable = TRUE
+        )
+        
+        self$linear_bias <- self$add_weight(
+          name = 'linear_bias',
+          shape = self$filters,
+          initializer = self$bias_initializer,
+          regularizer = self$bias_regularizer,
+          trainable = TRUE
+        )
+        
+        self$gated_kernel <- self$add_weight(
+          name = 'gated_kernel',
+          shape = self$kernel_shape,
+          initializer = self$kernel_initializer,
+          regularizer = self$kernel_regularizer,
+          trainable = TRUE
+        )
+        
+        self$gated_bias <- self$add_weight(
+          name = 'gated_bias',
+          shape = self$filters,
+          initializer = self$bias_initializer,
+          regularizer = self$bias_regularizer,
+          trainable = TRUE
+        )
+      },
+      
+      call = function(x, mask = NULL) {
+        
+        linear_out <-
+          tf$keras$backend$conv1d(x, self$linear_kernel, padding = 'same') %>%
+          tf$keras$backend$bias_add(self$linear_bias)
+        
+        gated_out <-
+          tf$keras$backend$conv1d(x, self$gated_kernel, padding = 'same') %>%
+          tf$keras$backend$bias_add(self$gated_bias)
+        
+        h <-
+          layer_multiply(list(
+            linear_out,
+            layer_activation(gated_out, activation = 'sigmoid')
+          ))
+        
+        h
+      },
+      
+      compute_output_shape = function(input_shape) {
+        list(NULL, input_shape[[2]], self$filters)
+      }
+    )
+  )
+
+
+layer_glu <- 
+  function(object,
+           filters = 32,
+           kernel_size = 3,
+           kernel_initializer = 'glorot_normal',
+           kernel_regularizer = 'l2',
+           bias_initializer = 'zeros',
+           bias_regularizer = NULL,
+           name = NULL,
+           trainable = TRUE) {
+    create_layer(
+      GatedLinearUnit,
+      object,
+      list(
+        filters = as.integer(filters),
+        kernel_size = as.integer(kernel_size),
+        kernel_initializer = tf$keras$initializers$get(kernel_initializer),
+        kernel_regularizer = tf$keras$regularizers$get(kernel_regularizer),
+        bias_initializer = tf$keras$initializers$get(bias_initializer),
+        bias_regularizer = tf$keras$regularizers$get(bias_regularizer),
+        name = name,
+        trainable = trainable
+      )
+    )
+  }
+
+
+
+
+GLUBlock <-
+  R6::R6Class(
+    "GLUBlock",
+    
+    inherit = KerasLayer,
+    
+    public = list(
+      num_layers = NULL,
+      filters = NULL,
+      kernel_size = NULL,
+      kernel_initializer = NULL,
+      kernel_regularizer = NULL,
+      bias_initializer = NULL,
+      bias_regularizer = NULL,
+      glu_layers = NULL,
+      
+      initialize = function(num_layers,
+                            filters,
+                            kernel_size,
+                            kernel_initializer,
+                            kernel_regularizer,
+                            bias_initializer,
+                            bias_regularizer) {
+        
+        self$num_layers <- num_layers
+        self$filters <- filters
+        self$kernel_size <- kernel_size
+        self$kernel_initializer <- kernel_initializer
+        self$kernel_regularizer <- kernel_regularizer
+        self$bias_initializer <- bias_initializer
+        self$bias_regularizer <- bias_regularizer
+      },
+      
+      build = function(input_shape) {
+
+        self$glu_layers <-
+          map(1L:self$num_layers, function(name) {
+            layer_glu(
+              filters = self$filters,
+              kernel_size = self$kernel_size,
+              kernel_initializer = self$kernel_initializer,
+              kernel_regularizer = self$kernel_regularizer,
+              bias_initializer = self$bias_initializer,
+              bias_regularizer = self$bias_regularizer,
+              name = paste0("glu_", name)
+            )
+          })
+      },
+      
+      call = function(x, mask = NULL) {
+
+        output <- x
+        
+        for (layer in self$glu_layers) output <- layer(output)
+        
+        residual <-
+          layer_conv_1d(x, filters = self$filters, kernel_size = 1)
+        
+        layer_add(c(output, residual))
+        
+      },
+      
+      compute_output_shape = function(input_shape) {
+        list(NULL, input_shape[[2]], self$filters)
+      },
+      
+      count_params = function() {
+        browser()
+
+        params <- 
+          sapply(self$layers, function(x) x$count_params()) %>% 
+          sum()
+        
+        params
+      }
+    )
+  )
+
+
+
+layer_glu_block <- 
+  function(object,
+           num_layers = 3,
+           filters = 32,
+           kernel_size = 3,
+           kernel_initializer = 'glorot_normal',
+           kernel_regularizer = 'l2',
+           bias_initializer = 'zeros',
+           bias_regularizer = NULL,
+           name = NULL,
+           trainable = TRUE) {
+    
+    create_layer(
+      GLUBlock,
+      object,
+      list(
+        num_layers = as.integer(num_layers),
+        filters = as.integer(filters),
+        kernel_size = as.integer(kernel_size),
+        kernel_initializer = tf$keras$initializers$get(kernel_initializer),
+        kernel_regularizer = tf$keras$regularizers$get(kernel_regularizer),
+        bias_initializer = tf$keras$initializers$get(bias_initializer),
+        bias_regularizer = tf$keras$regularizers$get(bias_regularizer),
+        name = name,
+        trainable = trainable
+      )
+    )
+  }
+
+
+
+make_glu_model <- 
+  function(winlen = 32768L, output_classes = vector(length = 5)) {
+    
+    input <- layer_input(shape = list(8192L, 6L))
+    
+    glu <- input %>% 
+      layer_glu(32, 3) %>% 
+      layer_glu(64, 5) %>% 
+      layer_glu(128, 8) %>% 
+      layer_glu(192, 12) %>% 
+      layer_glu(256, 16)
+    
+    output <- layer_global_max_pooling_1d(glu) %>% 
+      layer_dense(length(output_classes))
+    
+    build_and_compile(input, output)
+  }
+
+
+
+make_glu_block <- 
+  function(winlen = 32768L, output_classes = vector(length = 5)) {
+    input <- layer_input(shape = list(8192L, 6L))
+    
+    base <- input %>% 
+      layer_glu_block()
+    
+    output <- base %>% 
+      layer_global_max_pooling_1d() %>% 
+      layer_dense(length(output_classes), activation = 'softmax')
+    
+    build_and_compile(input, output)
+  }
+
+
